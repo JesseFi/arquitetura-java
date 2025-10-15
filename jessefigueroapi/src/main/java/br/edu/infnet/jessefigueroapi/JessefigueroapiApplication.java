361acd1287de0e07c32b27179cec6d0ddbe8cd56
Jesse Figuero <figuerojesse@gmail.com>
2025-10-15 20:32:45 -0300

    first commit

---
+++ jessefigueroapi/src/main/java/br/edu/infnet/jessefigueroapi/JessefigueroapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.jessefigueroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JessefigueroapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JessefigueroapiApplication.class, args);
+	}
+
+}
---
+++ jessefigueroapi/src/test/java/br/edu/infnet/jessefigueroapi/JessefigueroapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.jessefigueroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JessefigueroapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
